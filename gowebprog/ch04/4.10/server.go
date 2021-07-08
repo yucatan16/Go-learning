e5b4ce792f0e73d623459def451faa63b8c12a38
yucatan16 <yukako1160101@gmail.com>
2021-07-08 19:43:29 +0900

    study json and cookie

---
+++ gowebprog/ch04/4.10/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head>
+<body><h1>Hello World</h1></body>
+</html>`
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そのようなサービスはありません")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+func main() {
+	server  := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+
+	server.ListenAndServe()
+}
---
+++ gowebprog/ch04/4.8/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head>
+<body><h1>Hello World</h1></body>
+</html>`
+	w.Write([]byte(str))
+}
+
+func main() {
+	server  := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/write", process)
+	server.ListenAndServe()
+}
