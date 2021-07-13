ddefab38cbe56cbd74e6233a7f70e8bc92f3f4ba
yucatan16 <yukako1160101@gmail.com>
2021-07-13 22:53:51 +0900

    create record

---
+++ gowebprog/ch06/6.6/crud.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id int
+	Content string
+	Author string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hello World!", Author: "Yukako"}
+
+	fmt.Println(post)
+	post.Create()
+	fmt.Println(post)
+}
