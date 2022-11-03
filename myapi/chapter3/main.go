10db2a6afaff1ab97fd6f476982d7754a4828342
yucatty <k335283.yukako@gmail.com>
2022-11-03 23:44:16 +0900

    use QueryRow

--- myapi/chapter3/main.go
+++ myapi/chapter3/main.go
@@ -20,33 +20,29 @@ func main() {
 	}
 	defer db.Close()
 
+	articleID := 1
 	const sqlStr = `
 		select *
-		from articles;
+		from articles
+		where article_id = ?;
 	`
 
-	rows, err := db.Query(sqlStr)
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer rows.Close()
-
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		var createdTime sql.NullTime
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
-		}
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	fmt.Printf("%+v\n", article)
 }
