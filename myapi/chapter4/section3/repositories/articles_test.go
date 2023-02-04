2e1b4faa0451a922da8f7e097ec96fb438867bdc
yucatty <k335283.yukako@gmail.com>
2023-02-04 17:28:04 +0900

    chapter4 section3

---
+++ myapi/chapter4/section3/repositories/articles_test.go
@@ -0,0 +1,77 @@
+package repositories_test
+
+import (
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/yucatty/Go-learning/myapi/models"
+	"github.com/yucatty/Go-learning/myapi/repositories"
+)
+
+func TestSelectArticleList(t *testing.T) {
+	expectedNum := 2
+	got, err := repositories.SelectArticleList(testDB, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if num := len(got); num != expectedNum {
+		t.Errorf("want %d but got %d articles\n", expectedNum, num)
+	}
+}
+
+func TestSelectArticleDetail(t *testing.T) {
+	tests := []struct {
+		testTitle string
+		expected  models.Article
+	}{
+		{
+			testTitle: "subtest1",
+			expected: models.Article{
+				ID:       1,
+				Title:    "firstPost",
+				Contents: "This is my first blog",
+				UserName: "saki",
+				NiceNum:  2,
+			},
+		}, {
+			testTitle: "subtest2",
+			expected: models.Article{
+				ID:       2,
+				Title:    "2nd",
+				Contents: "Second blog post",
+				UserName: "saki",
+				NiceNum:  4,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			got, err := repositories.SelectArticleDetail(testDB, test.expected.ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got.ID != test.expected.ID {
+				t.Errorf("ID: get %d but want %d\n", got.ID, test.expected.ID)
+			}
+
+			if got.Title != test.expected.Title {
+				t.Errorf("Title: get %s but want %s\n", got.Title, test.expected.Title)
+			}
+
+			if got.Contents != test.expected.Contents {
+				t.Errorf("Contents: get %s but want %s\n", got.Contents, test.expected.Contents)
+			}
+
+			if got.UserName != test.expected.UserName {
+				t.Errorf("UserName: get %s but want %s\n", got.UserName, test.expected.UserName)
+			}
+			if got.NiceNum != test.expected.NiceNum {
+				t.Errorf("NiceNum: get %d but want %d\n", got.NiceNum, test.expected.NiceNum)
+			}
+
+		})
+	}
+}
