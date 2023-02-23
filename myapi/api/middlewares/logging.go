d0ae98981788cfeea4190533fc6078172483fcfb
yucatty <k335283.yukako@gmail.com>
2023-02-23 18:21:51 +0900

    add 9-3

--- myapi/api/middlewares/logging.go
+++ myapi/api/middlewares/logging.go
@@ -5,9 +5,28 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Println(req.RequestURI, req.Method)
-		next.ServeHTTP(w, req)
+
+		rlw := NewResLoggingWriter(w)
+
+		next.ServeHTTP(rlw, req)
+
+		log.Println("res: ", rlw.code)
 	})
 }
