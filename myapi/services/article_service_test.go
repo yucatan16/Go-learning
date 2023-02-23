10c158f0e2155072726902118544fc280f71c294
yucatty <k335283.yukako@gmail.com>
2023-02-23 23:40:32 +0900

    add 10-3

---
+++ myapi/services/article_service_test.go
@@ -0,0 +1,44 @@
+package services_test
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/yucatty/Go-learning/myapi/services"
+)
+
+var aSer *services.MyAppService
+
+func TestMain(m *testing.M) {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	aSer = services.NewMyAppService(db)
+
+	m.Run()
+}
+
+func BenchmarkGetArticleService(b *testing.B) {
+	articleID := 1
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := aSer.GetArticleService(articleID)
+		if err != nil {
+			b.Error(err)
+			break
+		}
+	}
+}
