ff3a75f4caa87541d22903e4627286d37c7f7e02
yucatty <k335283.yukako@gmail.com>
2023-02-24 00:24:24 +0900

    add 11-2

--- myapi/api/middlewares/logging.go
+++ myapi/api/middlewares/logging.go
@@ -21,12 +21,13 @@ func (rsw *resLoggingWriter) WriteHeader(code int) {
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		log.Println(req.RequestURI, req.Method)
+		traceID := newTraceID()
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
 
 		rlw := NewResLoggingWriter(w)
 
 		next.ServeHTTP(rlw, req)
 
-		log.Println("res: ", rlw.code)
+		log.Printf("[%d]res: %d", traceID, rlw.code)
 	})
 }
